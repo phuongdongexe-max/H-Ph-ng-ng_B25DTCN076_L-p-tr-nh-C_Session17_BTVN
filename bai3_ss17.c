a680b74bf921d142a826f1b41bbad18478e71205
phuongdongexe-max <phuongdongexe@gmail.com>
2025-11-13 09:59:43 +0700

    Update bai3_ss17.c

--- bai3_ss17.c
+++ bai3_ss17.c
@@ -5,12 +5,13 @@ int main() {
     char Array1[100];
     char Array2[50];
     printf("Nhap vao chuoi thu nhat: ");
-    fgets(Array1, sizeof(Array1), stdin);
+    fgets(Array1, 100, stdin);
     printf("Nhap vao chuoi thu hai: ");
-    fgets(Array2, sizeof(Array2), stdin);
+    fgets(Array2, 50, stdin);
     Array1[strcspn(Array1, "\n")] = '\0';
     Array2[strcspn(Array2, "\n")] = '\0';
     strcat(Array1, Array2);
     printf("Chuoi sau khi noi: %s\n", Array1);
     return 0;
 }
+
