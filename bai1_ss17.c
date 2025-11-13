c6f5fd623548cbb4603970216543f509c25b975e
phuongdongexe-max <phuongdongexe@gmail.com>
2025-11-13 09:48:27 +0700

    Update bai1_ss17.c

--- bai1_ss17.c
+++ bai1_ss17.c
@@ -6,20 +6,22 @@ int main (){
 	char arr[30];
     fgets(arr, 30, stdin);
     int i, j;
+	int flag = 1;
     arr[strcspn(arr, "\n")] = '\0';
     int len = strlen(arr);
-    for (i=0; i < len; i++){
-    	for (j = len; j < 1; j--){
-    		int cmp = strcmp(arr[i], arr[j]);
-	if (cmp != 0) {
-  		printf ("Khong phai la Palindrome.\n");
-	} else {
-		printf ("La chuoi Palindrome.\n");
-		}
+    for (i=0; i < len /2 ; i++){
+		if (arr[i] != arr[len -1 - i] ) {
+  		flag =0;
 		}
 	}
+		if (flag = 1){
+			printf ("Day la chuoi palindrome");
+		} else {
+			printf ("Day khong phai chuoi palidrome");
+		}
 	return 0;
 }
+
 
 
 
